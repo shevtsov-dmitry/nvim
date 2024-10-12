2eb71679ea346573e76a6cd29c1f4c4e213bfb71
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-10-12 11:05:07 +0300

    new perfect dark colorsheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,11 +1,22 @@
 -- ### THEME ###
 return {
+
+    {
+        'slugbyte/lackluster.nvim',
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+            vim.cmd.colorscheme 'lackluster'
+        end,
+    },
+
     -- {
     --     'navarasu/onedark.nvim',
     --     priority = 1000, -- Make sure to load this before all the other start plugins.
     --     opts = {
     --         transparent = true,
     --         term_colors = true, -- Change terminal color as per the selected theme style
+    --
     --     },
     --     init = function()
     --         -- vim.go.background = 'dark'
@@ -15,16 +26,18 @@ return {
     --         vim.opt.termguicolors = true
     --     end,
     -- },
-    {
-        'savq/melange-nvim',
-        priority = 1000,
-        init = function()
-            vim.cmd.colorscheme 'melange'
-            vim.opt.termguicolors = true
-            vim.opt.background = 'light'
-            -- vim.opt.background = 'dark'
-        end,
-    },
+
+    -- {
+    --     'savq/melange-nvim',
+    --     priority = 1000,
+    --     init = function()
+    --         vim.cmd.colorscheme 'melange'
+    --         vim.opt.termguicolors = true
+    --         -- vim.opt.background = 'light'
+    --         vim.opt.background = 'dark'
+    --     end,
+    -- },
+
     -- {
     --     'rose-pine/neovim',
     --     name = 'rose-pine',
@@ -41,17 +54,17 @@ return {
     --     end,
     -- },
 
-    {
-        'nyoom-engineering/oxocarbon.nvim',
-        config = function()
-            vim.opt.background = 'dark' -- set this to dark or light
-            -- vim.opt.background = 'light' -- set this to dark or light
-            vim.cmd 'colorscheme oxocarbon'
-            -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-            -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-        end,
-        -- event = foo,
-        -- config = bar
-        -- end,
-    },
+    -- {
+    --     'nyoom-engineering/oxocarbon.nvim',
+    --     config = function()
+    --         vim.opt.background = 'dark' -- set this to dark or light
+    --         -- vim.opt.background = 'light' -- set this to dark or light
+    --         vim.cmd 'colorscheme oxocarbon'
+    --         -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    --         -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    --     end,
+    --     -- event = foo,
+    --     -- config = bar
+    --     -- end,
+    -- },
 }
