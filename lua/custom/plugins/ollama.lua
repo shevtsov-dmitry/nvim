64a980642bf3b94ecc0f8f0e76fe89875974c06f
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-10-12 11:04:56 +0300

    ollama custom prompts for english learning

--- lua/custom/plugins/ollama.lua
+++ lua/custom/plugins/ollama.lua
@@ -10,19 +10,19 @@ return {
     keys = {
         -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
         {
-            '<leader>oo',
+            '<leader>o',
             ":<c-u>lua require('ollama').prompt()<cr>",
             desc = 'ollama prompt',
             mode = { 'n', 'v' },
         },
 
         -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-        {
-            '<leader>oG',
-            ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-            desc = 'ollama Generate Code',
-            mode = { 'n', 'v' },
-        },
+        -- {
+        --     '<leader>oG',
+        --     ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+        --     desc = 'ollama Generate Code',
+        --     mode = { 'n', 'v' },
+        -- },
     },
 
     ---@type Ollama.Config
@@ -38,12 +38,23 @@ return {
         -- },
         -- View the actual default prompts in ./lua/ollama/prompts.lua
         prompts = {
-            -- Sample_Prompt = {
-            --   prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
-            --   input_label = "> ",
-            --   model = "codegemma:7b",
-            --   action = "display",
-            -- },
+            -- example prompt
+            -- prompt = 'This is a sample prompt that receives $input and $sel(ection), among others.',
+            Explain_English = {
+                -- prompt = 'This is a sample prompt that receives $input and $sel(ection), among others.',
+                prompt = 'You are an english language teacher, I am not native with english language and you need to explain word or sentence to me simply clearly and easy. I ask you about clarifying $input in the context $sel',
+                input_label = '> ',
+                model = 'llama3.1',
+                action = 'display',
+            },
+
+            Translate_to_russian = {
+                -- prompt = 'This is a sample prompt that receives $input and $sel(ection), among others.',
+                prompt = 'You are a talanted and proficient russian translator. You need to translate this: $sel',
+                input_label = '> ',
+                model = 'llama3.1',
+                action = 'display',
+            },
         },
     },
 }
