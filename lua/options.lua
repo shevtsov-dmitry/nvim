23244a6880b5e72da241af0d21e15ecebbae7b5e
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-04-23 21:42:19 +0300

    fix: move content from keys to opts

---
+++ lua/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+-- vim.g.autoformat = true
+-- vim.g.lazyvim_picker = "telescope"
+
+opt.number = true
+opt.guicursor = ""
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.wrap = false
+opt.confirm = true 
+opt.ignorecase = true
+opt.smartcase = true
+opt.relativenumber = false 
+
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.updatetime = 250
+
